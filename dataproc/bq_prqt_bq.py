2ee9625b3d4820145f0a8273e6913dafa68b1257
NAVEEN KUMAR <69159734+naveen1908@users.noreply.github.com>
2023-02-24 16:58:39 +0530

    data proc job code bqtobq via parquet
    DataProc Job to load parquet data in BQ Table (6 cols mix of int, string, datetime) 
    Create a parquet file using python having 10k records (few duplicates)
    Put it in GCS
    Pyspark code to read parquet file → remove dups → load in BQ table
    
    Implementation Plan and steps:
    1.Project Name: ml-console-poc
    2.Dataproc Cluster Name: pysparkpoc 
    Create DataProc cluster with very low least memory configuration. using gcloud command. 
    3.Two table will be created (Input_Table, Output_Table)
        Input_Table(Native): Having 6 cols mix of int, string, datetime with 10k records with few duplicates records
        Output_Table(Native): Having 6 cols mix of int, string, datetime with Blank records
    
    4.A pyspark code for creating a parquet file and load it in GCS from the input table
        This pyspark code will run on "pysparkpoc" dataproc cluster and read data from "Input_Table", create parquet file and load it in GCS
    
    5.A Pyspark code to read parquet file → remove dups → load in BQ table
    
        This pyspark code will run on "pysparkpoc" dataproc cluster and read data from GCS parquet file, remove duplicates and load it in BQ "Output_Table"

---
+++ dataproc/bq_prqt_bq.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""BigQuery I/O PySpark example."""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+  .builder \
+  .master('yarn') \
+  .appName('spark-bigquery-demo') \
+  .getOrCreate()
+
+# Use the Cloud Storage bucket for temporary BigQuery export data used
+# by the connector.
+bucket = "nvn-bucket/staging"
+spark.conf.set('temporaryGcsBucket', bucket)
+
+# Load data from BigQuery.
+words = spark.read.format('bigquery') \
+  .option('table', 'mlconsole-poc.nvn_dataset.annonymuspar') \
+  .load()
+words.createOrReplaceTempView('words')
+
+# Perform word count.
+remove_dupl = spark.sql(
+    'SELECT DISTINCT * FROM nvn_dataset.annonymuspar')
+remove_dupl.show()
+remove_dupl.printSchema()
+
+# Saving the data to BigQuery
+remove_dupl.write.format('bigquery') \
+  .option('table', 'mlconsole-poc.nvn_dataset.annonymusparout') \
+  .save()
